c72bd5d882a254f9a7998d62f3d49b0e167fa621
dilin1212 <dilindinesan@gmail.com>
2017-05-03 23:19:04 +0300

    added array index method to locate the postion of array values

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,27 +11,31 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[] numbers = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            int a = 60;
+            int a = 100;
+            int location = Array.IndexOf(numbers, a) + 1;
             bool searchvalue = false;
+
 
             for (int i = 0; i < 10; i++)
             {
-
+               
                 if (numbers[i] == a)
                 {
-
+                   
                     searchvalue = true;
                     break;
                 }
             }
             if (searchvalue)
             {
-                Console.WriteLine("THE VALUE OF a is FOUND");
+                Console.WriteLine("THE VALUE OF 'a' is FOUND & it is located at position " + location + " of the array");
+              
             }
             else
             {
                 Console.WriteLine("The value a not found");
             }
+            Array.IndexOf(numbers, a);
             Console.ReadLine();
 
         }
