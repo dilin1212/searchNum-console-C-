f63016d06f43ebc569fb60d06e8978fd5ef30a12
dilin1212 <dilindinesan@gmail.com>
2017-05-04 00:27:04 +0300

    code reduced

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,31 +11,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[] numbers = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            int a = 100;
+            int a = 70;
             int location = Array.IndexOf(numbers, a) + 1;
-            bool searchvalue = false;
-
-
-            for (int i = 0; i < 10; i++)
-            {
-               
-                if (numbers[i] == a)
-                {
-                   
-                    searchvalue = true;
-                    break;
-                }
-            }
-            if (searchvalue)
-            {
-                Console.WriteLine("THE VALUE OF 'a' is FOUND & it is located at position " + location + " of the array");
-              
-            }
-            else
+            
+            if (location == 0)
             {
                 Console.WriteLine("The value a not found");
             }
-            Array.IndexOf(numbers, a);
+            else
+            {
+                Console.WriteLine("THE VALUE OF 'a' is FOUND & it is located at position " + location + " of the array");
+
+           }
+            
             Console.ReadLine();
 
         }
